be3ca8efd77b7cb5b58478de94654ae760eea387
MakoChe <maxmendezhdez@gmail.com>
2025-11-12 17:47:02 -0600

    Sum function C

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+void sum() {
+    int a, b;
+    scanf("%d", &a);	
+    scanf("%d", &b);
+	int res=a+b;
+    printf("%d",res);
+}
+
 int main() {
     printf("¡Hola, mundo!\n");
+    sum();
     return 0;
 }
