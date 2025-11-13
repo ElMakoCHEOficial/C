3bf1b1afa9ea6388339b938d72c8cf9a9cd0c81f
MakoChe <maxmendezhdez@gmail.com>
2025-11-12 19:38:11 -0600

    My Math lib C

--- main.c
+++ main.c
@@ -1,22 +1,14 @@
+// main.c
 #include <stdio.h>
-
-void sum() {
-    int a, b;
-    scanf("%d", &a);	
-    scanf("%d", &b);
-	int res=a+b;
-    printf("%d",res);
-}
-void multiply() {
-    int a, b;
-    scanf("%d", &a);	
-    scanf("%d", &b);
-    int res=a*b;
-    printf("%d",res);
-}
-
+#include "mymath.h"
 int main() {
-    printf("¡Hola, mundo!\n");
-    sum();
+    int g, m;
+    double s;
+    grados_a_dms(45.6789, &g, &m, &s);
+    printf("45.6789° = %d° %d' %.2f\"\n", g, m, s);
+    printf("¿7 es primo? %s\n", es_primo(7) ? "Sí" : "No");
+    printf("¿10 es primo? %s\n", es_primo(10) ? "Sí" : "No");
+    printf("1 rad = %.2f grados\n", radianes_a_grados(1.0));
+    printf("180 grados = %.2f rad\n", grados_a_radianes(180.0));
     return 0;
 }
