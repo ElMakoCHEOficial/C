cbd037361d255c183bd7e85d33b1250b8c6826c7
MakoChe <84734574+ElMakoCHEOficial@users.noreply.github.com>
2026-03-22 21:25:38 -0600

    Add Sum.c to calculate the sum of two integers

---
+++ Basic/Sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+#include <stdlib.h>
+int main(void){
+    int cant1=0,cant2=0;
+    // printf("Escribe el primer numero: ");
+    scanf("%d", &cant1);	
+    // printf("Escribe el segundo numero: ");
+    scanf("%d", &cant2);
+	int result=cant1+cant2;
+    printf("%d",result);
+    return 0;
+}
