ae8498301862ef9d2ff7729c51a4dff3cd3954cb
MakoChe <84734574+ElMakoCHEOficial@users.noreply.github.com>
2026-03-27 16:53:45 -0600

    Add longInt.c to multiply two long integers

---
+++ Basic/longInt.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+#include <stdlib.h>
+int main(void){
+    long long int num1,num2;
+    // printf("Escribe el primer lado: ");
+    scanf("%lld", &num1);
+    // printf("Escribe el segundo lado: ");
+    scanf("%lld", &num2);
+    printf("%lld", num2*num1);
+    return 0;
+}
