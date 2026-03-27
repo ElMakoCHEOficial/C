c91f540309dc3ab1d562870e7baea962d05d1bdc
MakoChe <84734574+ElMakoCHEOficial@users.noreply.github.com>
2026-03-27 16:51:23 -0600

    Add rectangle perimeter calculation in RectPerim.c
    Implement rectangle perimeter calculation.

---
+++ Basic/RectPerim.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+#include <stdlib.h>
+int main(void){
+    int lado1=0,lado2=1;
+    scanf("%d", &lado1);
+    scanf("%d", &lado2);
+    printf("%d", lado2*2+lado1*2);
+    return 0;
+}
